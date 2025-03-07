c6636d40b715aa382888d6055d65273b8b92ab8b
MariamSawera <mariamsawera@gmail.com>
2025-03-07 08:52:29 -0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,50 +1,46 @@
 import './App.css';
-import Alert from './components/Alert';
 import About from './components/About';
+import Alert from './components/Alert';
 import Navbar from './components/Navbar';
 import TextForm from './TextForm';
 import React, { useState } from 'react';
-function App() {
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
-  const [ mode , setMode ] = useState ('light'); //whether darkmode enabled or not
-  const [ alert, setAlert ] = useState(null);
+function App() {
+  const [mode, setMode] = useState('light'); // whether dark mode is enabled or not
+  const [alert, setAlert] = useState(null);
 
   const showAlert = (message, type) => {
-       setAlert({
-        msg: message,
-        type: type
-       }) 
-       setTimeout(()=> {
-          setAlert(null);
-       }, 1500);
-       
-       
-  }
-
+    setAlert({ msg: message, type: type });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
 
   const toggleMode = () => {
-    if(mode === 'light'){
-      setMode ('dark');
+    if (mode === 'light') {
+      setMode('dark');
       document.body.style.backgroundColor = '#042743';
       showAlert("Dark mode has been enabled", "success");
-    }
-    else {
-      setMode ('light');
-      document.body.style.backgroundColor ='white'; 
+    } else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
       showAlert("Light mode has been enabled", "success");
-         }
-  }
+    }
+  };
+
   return (
-    <>
-    <Navbar title="TextualMind"  mode={mode} toggleMode={toggleMode} />
-    <Alert alert = {alert}/>
-    <div className="container my-3">
-    <TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode}/>
-    </div>
-
-    </>
-
-  );    
+    <Router>
+      <Navbar title="TextualMind" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
+      <div className="container my-3">
+        <Routes>
+          <Route path="/" element={<TextForm showAlert={showAlert} heading="Try TextualMind - Word counter, character counter, Remove extra Spaces" mode={mode} />} />
+          <Route path="/about" element={<About mode={mode} />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
